a4523ba25580bf230a9f6ed2d42f71e3c5af22aa
gan <1535624169@qq.com>
2020-01-06 10:35:35 +0800

    2020/1/6 甘庆明 测试2

--- src/main/java/com/accp/action/o/Servlet.java
+++ src/main/java/com/accp/action/o/Servlet.java
@@ -1,5 +1,88 @@
 package com.accp.action.o;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.accp.biz.o.starsBiz;
+import com.accp.pojo.Stars;
+
+@RestController
+@RequestMapping("/api/xj") // 命名空间
 public class Servlet {
 
+	@Autowired
+	private starsBiz starsbiz;// 技工星际的biz
+	
+	/**
+	 * 查询所有星级
+	 * @return
+	 */
+	@PostMapping("query")
+	public List<Stars> query() {
+		List<Stars> list = starsbiz.query();
+		return list;
+	}
+	
+	@PostMapping("queryByid")
+	public Stars query(@RequestBody String id) {
+		Integer id2=Integer.parseInt(id);
+		Stars stars = starsbiz.queryByid(id2);
+		return stars;
+	}
+	@PostMapping("add")
+	public Map<String, String> querykc(@RequestBody Stars sta) {
+		Map<String, String> message = new HashMap<String, String>();
+		Stars stars = starsbiz.queryByid(sta.getStarsid());
+		if (stars == null) {
+			int count = starsbiz.addStars(sta);
+			message.put("code", "200");
+			message.put("msg", "ok");
+			return message;
+		} else {
+			message.put("code", "500");
+			message.put("msg", "no");
+			return message;
+		}
+
+	}
+	@PostMapping("upd")
+	public Map<String, String> update(@RequestBody Stars sta) {
+		Map<String, String> message = new HashMap<String, String>();
+		int count = starsbiz.update(sta);
+		if (count > 0) {
+			message.put("code", "200");
+			message.put("msg", "ok");
+			return message;
+		} else {
+			message.put("code", "500");
+			message.put("msg", "no");
+			return message;
+		}
+
+	}
+	@PostMapping("del")
+	public Map<String, String> dele(@RequestBody String sz[]) {
+		Map<String, String> message = new HashMap<String, String>();
+		int count = 0;
+		for (String string : sz) {
+			Integer zhi = Integer.parseInt(string);
+			starsbiz.removeByidStars(zhi);
+			count++;
+		}
+		if (count > 0) {
+			message.put("code", "200");
+			message.put("msg", "ok");
+			return message;
+		} else {
+			message.put("code", "500");
+			message.put("msg", "no");
+			return message;
+		}
+
+	}
 }
---
+++ src/test/java/com/accp/demo/OuyangXiaoyueText.java
@@ -0,0 +1,41 @@
+package com.accp.demo;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.accp.biz.o.starsBiz;
+import com.accp.pojo.Stars;
+/**
+ * 欧阳小粤的数据测试类
+ * @author Administrator
+ *
+ */
+@SpringBootTest
+public class OuyangXiaoyueText {
+
+	@Autowired
+	private starsBiz starsbiz;
+	/**
+	 * 测试遍历技工星际所有的数据
+	 */
+	@Test
+	public void query() {
+//		List<Stars> list=starsbiz.query();
+//		for (Stars stars : list) {
+//			System.out.println(stars);
+//		}
+		//新增
+//		Stars sta = new Stars(4,"四级技工",(float)500);
+//		int count = starsbiz.addStars(sta);
+//		System.out.println("新增结果值："+count);
+		
+		//删除
+//		int count = starsbiz.removeByidStars(4);
+//		System.out.println("删除的结果值："+count);
+	}
+	
+	
+}
