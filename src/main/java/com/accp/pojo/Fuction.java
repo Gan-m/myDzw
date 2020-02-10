e80ef174e11358f73526137330468749240bc81d
gan <1535624169@qq.com>
2020-02-10 10:24:27 +0800

    2020/2/10 甘庆明 放假第一次提交

---
+++ src/main/java/com/accp/pojo/Fuction.java
@@ -0,0 +1,57 @@
+package com.accp.pojo;
+
+public class Fuction {
+	private int fid;
+	
+	private String fno;
+	
+	private String fname;
+	
+	private int fpid;
+	
+	public int getFpid() {
+		return fpid;
+	}
+
+	public void setFpid(int fpid) {
+		this.fpid = fpid;
+	}
+
+	private Fuction fuc;
+
+	public int getFid() {
+		return fid;
+	}
+
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+
+	public String getFno() {
+		return fno;
+	}
+
+	public void setFno(String fno) {
+		this.fno = fno;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public Fuction getFuc() {
+		return fuc;
+	}
+
+	public void setFuc(Fuction fuc) {
+		this.fuc = fuc;
+	}
+
+	
+	
+	
+}
